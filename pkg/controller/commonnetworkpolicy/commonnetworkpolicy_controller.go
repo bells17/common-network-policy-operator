d5fc7605fd60b274dd9097ecd6d3094c22eaeaca
Daiki Hayakawa <bells171@gmail.com>
2018-11-09 12:58:49 +0900

    Write CRD Reconciler

--- pkg/controller/commonnetworkpolicy/commonnetworkpolicy_controller.go
+++ pkg/controller/commonnetworkpolicy/commonnetworkpolicy_controller.go
@@ -8,13 +8,17 @@ import (
 	"reflect"
 
 	commonnetworkpoliciesv1alpha1 "github.com/bells17/common-network-policy-operator/pkg/apis/commonnetworkpolicies/v1alpha1"
-	appsv1 "k8s.io/api/apps/v1"
+	customclientset "github.com/bells17/common-network-policy-operator/pkg/client/clientset/versioned"
 	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
@@ -37,7 +41,35 @@ func Add(mgr manager.Manager) error {
 
 // newReconciler returns a new reconcile.Reconciler
 func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	return &ReconcileCommonNetworkPolicy{Client: mgr.GetClient(), scheme: mgr.GetScheme()}
+	c, err := config.GetConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &ReconcileCommonNetworkPolicy{
+		Client:          mgr.GetClient(),
+		scheme:          mgr.GetScheme(),
+		clientSet:       newClientSet(c),
+		customClientSet: newCustomClientSet(c),
+	}
+}
+
+// newClientSet returns a new *Kubernetes.Clientset
+func newClientSet(c *rest.Config) *kubernetes.Clientset {
+	clientSet, err := kubernetes.NewForConfig(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return clientSet
+}
+
+func newCustomClientSet(c *rest.Config) customclientset.Interface {
+	cli, err := customclientset.NewForConfig(c)
+	if err != nil {
+		panic(err.Error())
+	}
+	return cli
 }
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
@@ -54,12 +86,32 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// TODO(user): Modify this to be the types you create
-	// Uncomment watch a Deployment created by CommonNetworkPolicy - change this for objects you create
-	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
+	// Uncomment watch a NetworkPolicy created by CommonNetworkPolicy - change this for objects you create
+	err = c.Watch(&source.Kind{Type: &networkingv1.NetworkPolicy{}}, &handler.EnqueueRequestForOwner{
 		IsController: true,
 		OwnerType:    &commonnetworkpoliciesv1alpha1.CommonNetworkPolicy{},
 	})
+	if err != nil {
+		return err
+	}
+
+	// watch a Namespace create event
+	mapFn := handler.ToRequestsFunc(
+		func(a handler.MapObject) []reconcile.Request {
+			return []reconcile.Request{
+				{NamespacedName: types.NamespacedName{
+					Name:      a.Meta.GetName(),
+					Namespace: a.Meta.GetNamespace(),
+				}},
+			}
+		})
+	// Watch Namespace and trigger Reconciles for objects
+	// mapped from the Namespace in the event
+	err = c.Watch(
+		&source.Kind{Type: &corev1.Namespace{}},
+		&handler.EnqueueRequestsFromMapFunc{
+			ToRequests: mapFn,
+		})
 	if err != nil {
 		return err
 	}
@@ -72,7 +124,9 @@ var _ reconcile.Reconciler = &ReconcileCommonNetworkPolicy{}
 // ReconcileCommonNetworkPolicy reconciles a CommonNetworkPolicy object
 type ReconcileCommonNetworkPolicy struct {
 	client.Client
-	scheme *runtime.Scheme
+	scheme          *runtime.Scheme
+	clientSet       kubernetes.Interface
+	customClientSet customclientset.Interface
 }
 
 // Reconcile reads that state of the cluster for a CommonNetworkPolicy object and makes changes based on the state read
@@ -84,70 +138,94 @@ type ReconcileCommonNetworkPolicy struct {
 // +kubebuilder:rbac:groups=networkingv1,resources=networkpolicy,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=commonnetworkpolicies.bells17.io,resources=commonnetworkpolicies,verbs=get;list;watch;create;update;patch;delete
 func (r *ReconcileCommonNetworkPolicy) Reconcile(request reconcile.Request) (reconcile.Result, error) {
-	// Fetch the CommonNetworkPolicy instance
-	instance := &commonnetworkpoliciesv1alpha1.CommonNetworkPolicy{}
-	err := r.Get(context.TODO(), request.NamespacedName, instance)
+	// Update all Common Network Policies
+	commonNetworkPolicies, err := r.customClientSet.CommonnetworkpoliciesV1alpha1().CommonNetworkPolicies().List(metav1.ListOptions{})
 	if err != nil {
-		if errors.IsNotFound(err) {
-			// Object not found, return.  Created objects are automatically garbage collected.
-			// For additional cleanup logic use finalizers.
-			return reconcile.Result{}, nil
+		return reconcile.Result{}, err
+	}
+
+	for _, commonNetworkPolicyItem := range commonNetworkPolicies.Items {
+
+		// Fetch the CommonNetworkPolicy instance
+		instance := &commonnetworkpoliciesv1alpha1.CommonNetworkPolicy{}
+		err := r.Get(context.TODO(), types.NamespacedName{
+			Name:      commonNetworkPolicyItem.ObjectMeta.Name,
+			Namespace: commonNetworkPolicyItem.ObjectMeta.Namespace,
+		}, instance)
+
+		if err != nil {
+			if !errors.IsNotFound(err) {
+				return reconcile.Result{}, err
+			}
 		}
-		// Error reading the object - requeue the request.
-		return reconcile.Result{}, err
-	}
 
-	// TODO(user): Change this to be the object type created by your controller
-	// Define the desired Deployment object
-	deploy := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      instance.Name + "-deployment",
-			Namespace: "default",
-		},
-		Spec: appsv1.DeploymentSpec{
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{"deployment": instance.Name + "-deployment"},
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"deployment": instance.Name + "-deployment"}},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:  "nginx",
-							Image: "nginx",
-						},
-					},
-				},
-			},
-		},
-	}
-	if err := controllerutil.SetControllerReference(instance, deploy, r.scheme); err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// TODO(user): Change this for the object type created by your controller
-	// Check if the Deployment already exists
-	found := &appsv1.Deployment{}
-	err = r.Get(context.TODO(), types.NamespacedName{Name: deploy.Name, Namespace: deploy.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		log.Printf("Creating Deployment %s/%s\n", deploy.Namespace, deploy.Name)
-		err = r.Create(context.TODO(), deploy)
+		namespaceList, err := r.clientSet.CoreV1().Namespaces().List(metav1.ListOptions{})
 		if err != nil {
 			return reconcile.Result{}, err
+		}
+
+		for _, item := range namespaceList.Items {
+			ns := item.ObjectMeta.Name
+
+			isExclude := false
+			for _, n := range instance.Spec.ExcludeNamespaces {
+				if n == ns {
+					isExclude = true
+				}
+			}
+			if isExclude {
+				continue
+			}
+
+			name := commonNetworkPolicyItem.Name
+			if commonNetworkPolicyItem.Spec.NamePrefix != "" {
+				name = instance.Spec.NamePrefix + "-" + name
+			}
+
+			networkPolicy := &networkingv1.NetworkPolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      name,
+					Namespace: ns,
+				},
+				Spec: commonNetworkPolicyItem.Spec.PolicySpec,
+			}
+			err := r.applyNetworkPolicy(instance, networkPolicy)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+
+		}
+	}
+
+	return reconcile.Result{}, nil
+}
+
+// applyNetworkPolicy apply the networkPolicy
+func (r *ReconcileCommonNetworkPolicy) applyNetworkPolicy(instance *commonnetworkpoliciesv1alpha1.CommonNetworkPolicy, networkPolicy *networkingv1.NetworkPolicy) error {
+	err := controllerutil.SetControllerReference(instance, networkPolicy, r.scheme)
+	if err != nil {
+		return err
+	}
+
+	found := &networkingv1.NetworkPolicy{}
+	err = r.Get(context.TODO(), types.NamespacedName{Name: networkPolicy.Name, Namespace: networkPolicy.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		log.Printf("Creating Network Policy %s/%s\n", networkPolicy.Namespace, networkPolicy.Name)
+		err = r.Create(context.TODO(), networkPolicy)
+		if err != nil {
+			return err
 		}
 	} else if err != nil {
-		return reconcile.Result{}, err
+		return err
 	}
 
-	// TODO(user): Change this for the object type created by your controller
-	// Update the found object and write the result back if there are any changes
-	if !reflect.DeepEqual(deploy.Spec, found.Spec) {
-		found.Spec = deploy.Spec
-		log.Printf("Updating Deployment %s/%s\n", deploy.Namespace, deploy.Name)
+	if !reflect.DeepEqual(networkPolicy.Spec, found.Spec) {
+		log.Printf("Updating Network Policy %s/%s\n", networkPolicy.Namespace, networkPolicy.Name)
+		found.Spec = networkPolicy.Spec
 		err = r.Update(context.TODO(), found)
 		if err != nil {
-			return reconcile.Result{}, err
+			return err
 		}
 	}
-	return reconcile.Result{}, nil
+	return nil
 }
